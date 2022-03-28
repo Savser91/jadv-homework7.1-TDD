2b39ccf5366c0294b2e2d23c0a03de6620af5f8d
Savser91 <savser91@gmail.com>
2022-03-28 23:31:29 +0300

    writing code

--- src/main/java/CreditCalculator.java
+++ src/main/java/CreditCalculator.java
@@ -6,14 +6,24 @@ public class CreditCalculator {
     }
 
     public int getMonthPayment(BigDecimal creditSum, BigDecimal percent, int lengthInMonths) {
-        return null;
+        BigDecimal monthlyRate = percent.divide(new BigDecimal("12"));
+        BigDecimal pow = monthlyRate.add(new BigDecimal(1)).pow(-lengthInMonths, MathContext.DECIMAL64);
+        BigDecimal denominator = new BigDecimal(1);
+        denominator = denominator.subtract(pow);
+
+        BigDecimal amount = creditSum.multiply(monthlyRate).divide(
+                denominator, RoundingMode.UP
+        );
+        return amount.intValue();
     }
 
     public int getFullSum(BigDecimal creditSum, BigDecimal percent) {
-        return null;
+        BigDecimal result = creditSum.multiply(percent).add(creditSum);
+        return result.intValue();
     }
 
     public int getOverpay(BigDecimal creditSum, BigDecimal percent) {
-        return null;
+        BigDecimal result = creditSum.multiply(percent);
+        return result.intValue();
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,9 @@ import java.math.BigDecimal;
 
 public class Main {
     public static void main(String[] args) {
-        
+        CreditCalculator calculator = new CreditCalculator();
+        System.out.println(calculator.getMonthPayment(new BigDecimal("200000"), new BigDecimal("0.12"), 24));
+        System.out.println(calculator.getFullSum(new BigDecimal("1000000"), new BigDecimal("0.097")));
+        System.out.println(calculator.getOverpay(new BigDecimal("1000000"), new BigDecimal("0.097")));
     }
 }
